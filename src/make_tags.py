eb98a597bfad1f5e9fd844eda8f808d637da63c6
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-10-27 10:17:19 -0400

    change to python3

--- src/make_tags.py
+++ src/make_tags.py
@@ -1,34 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # script to generate tag urdf from yaml file
 
 import yaml
 import argparse
-import sys
 
 
-def tags_to_urdf(tags, mesh_dir='package://tagslam_viz/tags', tag_fam='36_11', global_link='map', robot_name='tags', tag_thick=0.005):
-    print "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-    print "<robot name=\"%s\">" % robot_name
-    print "<link name=\"%s\"/>" % global_link
+def tags_to_urdf(tags, mesh_dir='package://tagslam_viz/tags', tag_fam='36_11',
+                 global_link='map', robot_name='tags', tag_thick=0.005):
+    print("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+    print("<robot name=\"%s\">" % robot_name)
+    print("<link name=\"%s\"/>" % global_link)
     for t in tags:
         sz = float(t['size'])
         tid = int(t['id'])
-        print " <link name=\"tag_%d\">" % tid
-        print "  <visual>"
-        print "   <origin rpy=\"0 0 0\" xyz=\"0 0 -%f\"/>" % (sz * tag_thick)
-        print "     <geometry>"
-        print "       <mesh filename=\"%s/tag%s_%05d.dae\" scale=\"%f %f %f\"/>" % (
-            mesh_dir, tag_fam, int(t['id']), sz, sz, sz)
-        print "     </geometry>"
-        print "  </visual>"
-        print " </link>"
-        print " <joint name=\"tag_%d_to_world\" type=\"fixed\">" % tid
-        print "   <parent link=\"%s\"/>" % global_link
-        print "   <child link=\"tag_%d\"/>" % tid
-        print "   <origin xyz=\"0 0 0\" rpy=\"0 0 0\"/>"
-        print "   </joint>"
-    print "</robot>"
+        print(" <link name=\"tag_%d\">" % tid)
+        print("  <visual>")
+        print("   <origin rpy=\"0 0 0\" xyz=\"0 0 -%f\"/>" % (sz * tag_thick))
+        print("     <geometry>")
+        print("       <mesh filename=\"%s/tag%s_%05d.dae\" scale=\"%f %f %f\"/>" % (
+            mesh_dir, tag_fam, int(t['id']), sz, sz, sz))
+        print("     </geometry>")
+        print("  </visual>")
+        print(" </link>")
+        print(" <joint name=\"tag_%d_to_world\" type=\"fixed\">" % tid)
+        print("   <parent link=\"%s\"/>" % global_link)
+        print("   <child link=\"tag_%d\"/>" % tid)
+        print("   <origin xyz=\"0 0 0\" rpy=\"0 0 0\"/>")
+        print("   </joint>")
+    print("</robot>")
 
 
 def find_tags(d):
@@ -45,12 +45,14 @@ def find_tags(d):
                 all = all + find_tags(v) # recursion!
     return all
 
+
 def read_yaml(filename):
     with open(filename, 'r') as y:
         try:
-            return yaml.load(y)
+            return yaml.safe_load(y)
         except yaml.YAMLError as e:
             print(e)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
